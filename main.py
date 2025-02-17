cc06a34bc4e59a18e64be83a57c26dd61d0ff45d
Davidk0Fiddler <111727231+Davidk0Fiddler@users.noreply.github.com>
2025-02-17 14:58:16 +0100

    Base Game Display (Done)
    Base Game Display (Done)

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 import pygame
-from setting import *
+from settings import *
 from colors import *
 
-
-current_application_width = application_window_width1
-current_application_height = application_window_height2
 pygame.init()
 
+current_application_width = application_window_width1
+current_application_height = application_window_height1
 
-application_window = pygame.display.set(current_application_width, current_application_height)
+def displaying_methods(application_window):
+    application_window.fill(RGBColors.black)
+    pygame.display.flip()    
+
+application_window = pygame.display.set_mode((current_application_width, current_application_height))
 
 while application_running:
-    application_window.fill(RGBColors.black)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             application_running = False
-
-    pygame.display.flip()    
-
+    
+    displaying_methods(application_window)
 pygame.quit()
