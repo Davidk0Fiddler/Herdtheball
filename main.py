25ad6e18338bf6c285a6d7a508cb39d150711294
Davidk0Fiddler <111727231+Davidk0Fiddler@users.noreply.github.com>
2025-03-31 14:47:56 +0200

    Window size 1
    Ablak méret fügvényes megadása

--- main.py
+++ main.py
@@ -4,14 +4,25 @@ from colors import *
 
 pygame.init()
 
-current_application_width = application_window_width1
-current_application_height = application_window_height1
+size_number = 1
+
+def selecting_size(number):
+    current_application_width = 0
+    current_application_height = 0  
+
+    selector_w = f'current_application_width = application_window_width{number}'
+    selector_h = f'current_application_height = application_window_height{number}'
+
+    exec(selector_w)
+    exec(selector_h)
+
+    return (current_application_width, current_application_height)
 
 def displaying_methods(application_window):
     application_window.fill(RGBColors.black)
     pygame.display.flip()    
 
-application_window = pygame.display.set_mode((current_application_width, current_application_height))
+application_window = pygame.display.set_mode(selecting_size(size_number))
 
 while application_running:
     for event in pygame.event.get():
